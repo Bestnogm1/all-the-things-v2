3ba9cfb62eb2bafae9cc65e6c8f76cea3ebbcd2f
Bestnogm1 <koffiarielhessou@gmail.com>
2022-03-24 10:46:15 -0400

    done

--- src/components/ThingCard/ThingCard.jsx
+++ src/components/ThingCard/ThingCard.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const ThingCard = ({ thing }) => {
   return (
     <>
+    <h1> hi i was here ariel </h1>
       <h2>{thing.name}</h2>
       <img height='300'src={thing.image} alt=""/>
       <ul>
         {thing.attributes.map((attribute, idx) =>
           <li key={idx} >{attribute}</li>  
+          
         )}
       </ul>
     </>
